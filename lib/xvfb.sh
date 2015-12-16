9d7a97f12c62f53362a0e473777e820cdf13277c
Johnathan Leppert <johnathan@kespry.com>
2015-12-15 17:38:07 -0800

    xvfb

---
+++ lib/xvfb.sh
@@ -0,0 +1,2 @@
+sudo apt-get install -y build-essential clang libdbus-1-dev libgtk2.0-dev libnotify-dev libgnome-keyring-dev libgconf2-dev libasound2-dev libcap-dev libcups2-dev libxtst-dev libxss1 libnss3-dev gcc-multilib g++-multilib libasound2
+sudo apt-get install -y xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-scalable xfonts-cyrillic x11-apps
